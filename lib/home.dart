82f6fa492a12ecafd09655ff456ba172df832e98
kashitsuwa <cerberusprogrammer@gmail.com>
2022-12-28 13:11:18 -0800

    New things...

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quotes/quotes.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,14 +11,30 @@ class Home extends StatefulWidget {
 class _Home extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    String quoteContent = 'uwu';
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inspire Me'),
       ),
-      body: const Center(),
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          quoteContent,
+          style: const TextStyle(
+            fontFamily: 'Roboto',
+            backgroundColor: Color.fromARGB(255, 244, 157, 6),
+            letterSpacing: 1,
+            fontStyle: FontStyle.italic,
+            fontSize: 20,
+          ),
+          textAlign: TextAlign.justify,
+        ),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: (() {}),
-        child: const Icon(Icons.person),
+        child: const Icon(Icons.refresh),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,12 @@ import 'home.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Inspire Me',
       theme: ThemeData(
-        brightness: Brightness.light,
+        brightness: Brightness.dark,
+        primarySwatch: Colors.amber,
+        useMaterial3: true,
       ),
       home: const Home(),
     ),
