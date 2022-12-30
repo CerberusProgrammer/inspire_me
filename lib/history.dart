0da8fbe84f5c2a92bd483bcec70d5e22978af3b9
Omar Flores <cerberusprogrammer@gmail.com>
2022-12-29 23:21:13 -0800

    A little add.

--- lib/history.dart
+++ lib/history.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class History extends StatefulWidget {
+  static List<Widget> historyQuotes = [];
+
   @override
   State<StatefulWidget> createState() => _History();
 }
